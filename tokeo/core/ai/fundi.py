3cdfe5171d15d8e0ed825056d69502a69777f4d8
Tom Freudenberg <tom.freudenberg@4commerce.de>
2026-07-25 03:20:04 +0200

    add: licence reference to classes

--- tokeo/core/ai/fundi.py
+++ tokeo/core/ai/fundi.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2026 Tom (Thomas) Freudenberg <th.freudenberg@gmail.com>
+#
+# SPDX-License-Identifier: LicenseRef-Tokeo-Fundi-Source-Available-1.0
+#
+# This file is part of Tokeo-Fundi and is licensed under the
+# Tokeo-Fundi Source-Available License 1.0. See the LICENSE.md file in the
+# project root for the full license text. Use is subject to its conditions.
+
 try:
     from cement.utils.version import get_version as cement_get_version
 except ModuleNotFoundError:
